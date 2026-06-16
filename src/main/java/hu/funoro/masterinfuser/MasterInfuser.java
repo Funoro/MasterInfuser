48049123b53c6cbcac5767d3b50d3b5e08010ff4
Hunor Csuhai <team3csh@gmail.com>
2026-06-16 21:41:01 +0200

    main modfile cleanup

--- src/main/java/hu/funoro/masterinfuser/MasterInfuser.java
+++ src/main/java/hu/funoro/masterinfuser/MasterInfuser.java
@@ -17,30 +17,21 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.config.ModConfig;
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.neoforged.fml.loading.FMLEnvironment;
 import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.server.ServerStartingEvent;
 import org.slf4j.Logger;
 
-// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(MasterInfuser.MOD_ID)
+@EventBusSubscriber(modid = MasterInfuser.MOD_ID)
 public class MasterInfuser {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "masterinfuser";
-    // Directly reference a slf4j logger
     public static final Logger LOGGER = LogUtils.getLogger();
 
-    // The constructor for the mod class is the first code that is run when your mod is loaded.
-    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
     public MasterInfuser(IEventBus bus, ModContainer modContainer) {
-        // Register the commonSetup method for modloading
-        bus.addListener(this::commonSetup);
-
         ModCreativeModeTabs.register(bus);
-
         ModItems.register(bus);
         ModBlocks.register(bus);
         ModMenuTypes.REGISTRY.register(bus);
@@ -56,37 +47,15 @@ public class MasterInfuser {
             bus.register(new ModMenuScreens());
             NeoForge.EVENT_BUS.register(new ClientRecipeHandler());
         }
-
-        NeoForge.EVENT_BUS.register(this);
-
-
-        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
-        modContainer.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
     }
 
     public static Identifier resource(String path) {
         return Identifier.fromNamespaceAndPath(MOD_ID, path);
     }
 
-    private void commonSetup(final FMLCommonSetupEvent event) {
+    @SubscribeEvent
+    public static void onCommonSetup(final FMLCommonSetupEvent event) {
         NeoForge.EVENT_BUS.register(RecipeIngredientCache.INSTANCE);
         LOGGER.info("Master Infuser mod loaded.");
     }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-
-    }
-
-    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-//    @EventBusSubscriber(modid = MOD_ID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
-//    public static class ClientModEvents {
-//        @SubscribeEvent
-//        public static void onClientSetup(FMLClientSetupEvent event) {
-//            // Some client setup code
-//            LOGGER.info("HELLO FROM CLIENT SETUP");
-//            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
-//        }
-//    }
 }
